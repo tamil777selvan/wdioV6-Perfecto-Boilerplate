7e19cc5c7e0a7f98e62e721cf70f08c73f53e9b5
Tamil Selvan <tamil777selvan@gmail.com>
2020-05-13 21:02:53 +0530

    Initial Commit

--- tests/screen/android/calculator.screen.js
+++ tests/screen/android/calculator.screen.js
@@ -3,7 +3,7 @@ class CalculatorScreen {
        this.number = number => `//*[@resource-id="com.sec.android.app.popupcalculator:id/bt_0${number}"]`;
        this.add = '//*[@resource-id="com.sec.android.app.popupcalculator:id/bt_add"]';
        this.equal = '//*[@resource-id="com.sec.android.app.popupcalculator:id/bt_equal"]';
-       this.result = '//*[@resource-id="com.sec.android.app.popupcalculator:id/txtCalc" or @resource-id="com.sec.android.app.popupcalculator:id/calc_edt]';
+       this.result = '//*[@resource-id="com.sec.android.app.popupcalculator:id/txtCalc" or @resource-id="com.sec.android.app.popupcalculator:id/calc_edt"]';
        this.clear = '//*[@resource-id="com.sec.android.app.popupcalculator:id/bt_clear"]';
    }
 }
